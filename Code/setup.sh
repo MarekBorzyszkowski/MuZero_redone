b9b30f13bd4206be5d55d545bb609b84c4677de4
Marek Borzyszkowski <marek.borzyszkowski@pg.edu.pl>
2025-01-03 23:21:10 +0100

    better settings

--- Code/setup.sh
+++ Code/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ ! -d venv ]; then
 	echo "Start of creating venv"
-	python3.7 -m venv venv
+	python3.10 -m venv venv
 	echo "venv created"
 	echo "Start of installing python packages"
 	source venv/bin/activate
-	pip install -r requirements_guzw.txt
+	pip install -r requirements.txt
 	deactivate
 	echo "End of installing python packages"
 else
